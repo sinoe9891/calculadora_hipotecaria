1cdad6680d7eeaca8322a0f0982d36e1c3b39083
sinoe9891 <sinoeproducciones@gmail.com>
2020-12-17 21:04:22 -0600

    optimizacion

--- js/js.js
+++ js/js.js
@@ -25,27 +25,19 @@ function calc() {
 	if (condominio === 'cond1') {
 
 		if (prima === 'prima1') {
-			calculoPrima(precio1, prima1);
-			montoFinanciar(precio1, prima1);
-			calculoCuota(tasas1, precio1, prima1);
+			functionAll(precio1, prima1, tasas1)
 		}
 
 		if (prima === 'prima2') {
-			calculoPrima(precio1, prima2);
-			montoFinanciar(precio1, prima2);
-			calculoCuota(tasas1, precio1, prima2);
+			functionAll(precio1, prima2, tasas1)
 		}
 
 		if (prima === 'prima3') {
-			calculoPrima(precio1, prima3);
-			montoFinanciar(precio1, prima3);
-			calculoCuota(tasas1, precio1, prima3);
+			functionAll(precio1, prima3, tasas1)
 		}
 
 		if (prima === 'prima4') {
-			calculoPrima(precio1, prima4);
-			montoFinanciar(precio1, prima4);
-			calculoCuota(tasas1, precio1, prima4);
+			functionAll(precio1, prima4, tasas1)
 		}
 	}
 
@@ -53,27 +45,19 @@ function calc() {
 	if (condominio === 'cond2') {
 
 		if (prima === 'prima1') {
-			calculoPrima(precio2, prima1);
-			montoFinanciar(precio2, prima1);
-			calculoCuota(tasas2, precio2, prima1);
+			functionAll(precio2, prima1, tasas2)
 		}
 
 		if (prima === 'prima2') {
-			calculoPrima(precio2, prima2);
-			montoFinanciar(precio2, prima2);
-			calculoCuota(tasas2, precio2, prima2);
+			functionAll(precio2, prima2, tasas2)
 		}
 
 		if (prima === 'prima3') {
-			calculoPrima(precio2, prima3);
-			montoFinanciar(precio2, prima3);
-			calculoCuota(tasas2, precio2, prima3);
+			functionAll(precio2, prima3, tasas2)
 		}
 
 		if (prima === 'prima4') {
-			calculoPrima(precio2, prima4);
-			montoFinanciar(precio2, prima4);
-			calculoCuota(tasas2, precio2, prima4);
+			functionAll(precio2, prima4, tasas2)
 		}
 	}
 
@@ -81,27 +65,19 @@ function calc() {
 	if (condominio === 'cond3') {
 
 		if (prima === 'prima1') {
-			calculoPrima(precio3, prima1);
-			montoFinanciar(precio3, prima1);
-			calculoCuota(tasas3, precio3, prima1);
+			functionAll(precio3, prima1, tasas3)
 		}
 
 		if (prima === 'prima2') {
-			calculoPrima(precio3, prima2);
-			montoFinanciar(precio3, prima2);
-			calculoCuota(tasas3, precio3, prima2);
+			functionAll(precio3, prima2, tasas3)
 		}
 
 		if (prima === 'prima3') {
-			calculoPrima(precio3, prima3);
-			montoFinanciar(precio3, prima3);
-			calculoCuota(tasas3, precio3, prima3);
+			functionAll(precio3, prima3, tasas3)
 		}
 
 		if (prima === 'prima4') {
-			calculoPrima(precio3, prima4);
-			montoFinanciar(precio3, prima4);
-			calculoCuota(tasas3, precio3, prima4);
+			functionAll(precio3, prima4, tasas3)
 		}
 	}
 
@@ -109,27 +85,19 @@ function calc() {
 	if (condominio === 'cond4') {
 
 		if (prima === 'prima1') {
-			calculoPrima(precio4, prima1);
-			montoFinanciar(precio4, prima1);
-			calculoCuota(tasas4, precio4, prima1);
+			functionAll(precio4, prima1, tasas4)
 		}
 
 		if (prima === 'prima2') {
-			calculoPrima(precio4, prima2);
-			montoFinanciar(precio4, prima2);
-			calculoCuota(tasas4, precio4, prima2);
+			functionAll(precio4, prima2, tasas4)
 		}
 
 		if (prima === 'prima3') {
-			calculoPrima(precio4, prima3);
-			montoFinanciar(precio4, prima3);
-			calculoCuota(tasas4, precio4, prima3);
+			functionAll(precio4, prima3, tasas4)
 		}
 
 		if (prima === 'prima4') {
-			calculoPrima(precio4, prima4);
-			montoFinanciar(precio4, prima4);
-			calculoCuota(tasas4, precio4, prima4);
+			functionAll(precio4, prima4, tasas4)
 		}
 	}
 
@@ -150,7 +118,6 @@ function calc() {
 	};
 
 	function calculoCuota(tasa, precio, prima){
-
 		var intereses = tasa/12;
 		var resultado = precio - (precio * prima);
 		var otro = intereses * resultado;
@@ -160,8 +127,13 @@ function calc() {
 
 		imprimirTotalPagar = document.getElementById('cuota').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(totalPagar.toFixed(2));
 		porcentajeAnual = document.getElementById('porcAnual').innerHTML = tasa*100 + "%";
+
 		return imprimirTotalPagar;
 	};
 
-
+	function functionAll(precio, prima, tasa){
+		calculoPrima(precio, prima);
+		montoFinanciar(precio, prima);
+		calculoCuota(tasa, precio, prima);
+	}
 }
