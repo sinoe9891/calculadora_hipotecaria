1f516ed6a44aaea7df1cdb018980567bbe5df9a5
sinoe9891 <sinoeproducciones@gmail.com>
2020-12-17 19:22:20 -0600

    calculo con tasas

--- js/js.js
+++ js/js.js
@@ -7,15 +7,19 @@ function calc() {
 	var precio4 = 99500.00
 
 	//Prima
-	var prima1 = 0.105;
-	var prima2 = 0.110;
-	var prima3 = 0.115;
-	var prima4 = 0.120;
+	var prima1 = 0.10;
+	var prima2 = 0.15;
+	var prima3 = 0.20;
+	var prima4 = 0.25;
 
+	//Tasas
+	var tasas1 = 0.105;
+	var tasas2 = 0.110;
+	var tasas3 = 0.115;
+	var tasas4 = 0.120;
 
 	var condominio = document.getElementById('condominios').value;
 	var prima = document.getElementById('primas').value;
-	var tasa = document.getElementById('tasas').value;
 
 	//Condominios
 
@@ -23,48 +27,75 @@ function calc() {
 	if (condominio === 'cond1') {
 
 		if (prima === 'prima1') {
-			var intereses = prima1/12;
-			console.log("intereses " + intereses);
-
+			//Prima
+			var resultadoPrima = precio1 * prima1;
+			//calculo de cuota
+			var intereses = tasas1/12;
 			var resultado = precio1 - (precio1 * prima1);
-			console.log("resultado " + resultado);
-
 			var otro = intereses * resultado;
-			console.log("multi " + otro);
-			
 			var calculo1 = 1+intereses;
 			var calculo2 = 1-Math.pow(calculo1,-240);
-			console.log(calculo1);
-			
-			console.log(calculo2);
 			var totalPagar =  otro / calculo2;
-
-			//var im2 = Math.pow((1 + im), -(m * n));
-
+			//Prima
+			document.getElementById('resultadoPrima').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(resultadoPrima.toFixed(2));
+			//Monto a Financiar
 			document.getElementById('result').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(resultado.toFixed(2));
 			//Cuota
-			
-			var deuda = precio1-resultado;
-			console.log("deuda " + deuda);
-		
-			var cuota = (intereses * deuda);
-			console.log("cuota " + cuota);
 			document.getElementById('cuota').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(totalPagar.toFixed(2));
 		}
 
 		if (prima === 'prima2') {
-			var resultado = precio1 * prima2;
+			//Prima
+			var resultadoPrima = precio1 * prima2;
+			//calculo de cuota
+			var intereses = tasas1/12;
+			var resultado = precio1 - (precio1 * prima2);
+			var otro = intereses * resultado;
+			var calculo1 = 1+intereses;
+			var calculo2 = 1-Math.pow(calculo1,-240);
+			var totalPagar =  otro / calculo2;
+			//Prima
+			document.getElementById('resultadoPrima').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(resultadoPrima.toFixed(2));
+			//Monto a Financiar
 			document.getElementById('result').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(resultado.toFixed(2));
+			//Cuota
+			document.getElementById('cuota').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(totalPagar.toFixed(2));
 		}
 
 		if (prima === 'prima3') {
-			var resultado = precio1 * prima3;
+			//Prima
+			var resultadoPrima = precio1 * prima3;
+			//calculo de cuota
+			var intereses = tasas1/12;
+			var resultado = precio1 - (precio1 * prima3);
+			var otro = intereses * resultado;
+			var calculo1 = 1+intereses;
+			var calculo2 = 1-Math.pow(calculo1,-240);
+			var totalPagar =  otro / calculo2;
+			//Prima
+			document.getElementById('resultadoPrima').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(resultadoPrima.toFixed(2));
+			//Monto a Financiar
 			document.getElementById('result').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(resultado.toFixed(2));
+			//Cuota
+			document.getElementById('cuota').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(totalPagar.toFixed(2));
 		}
 
 		if (prima === 'prima4') {
-			var resultado = precio1 * prima4;
+			//Prima
+			var resultadoPrima = precio1 * prima4;
+			//calculo de cuota
+			var intereses = tasas1/12;
+			var resultado = precio1 - (precio1 * prima4);
+			var otro = intereses * resultado;
+			var calculo1 = 1+intereses;
+			var calculo2 = 1-Math.pow(calculo1,-240);
+			var totalPagar =  otro / calculo2;
+			//Prima
+			document.getElementById('resultadoPrima').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(resultadoPrima.toFixed(2));
+			//Monto a Financiar
 			document.getElementById('result').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(resultado.toFixed(2));
+			//Cuota
+			document.getElementById('cuota').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(totalPagar.toFixed(2));
 		}
 	}
 
@@ -72,23 +103,79 @@ function calc() {
 	if (condominio === 'cond2') {
 
 		if (prima === 'prima1') {
-			var resultado = precio2 * prima1;
+			//Prima
+			var resultadoPrima = precio2 * prima1;
+			console.log(resultadoPrima);
+			//calculo de cuota
+			var intereses = tasas2/12;
+			var resultado = precio2 - (precio2 * prima1);
+			var otro = intereses * resultado;
+			var calculo1 = 1+intereses;
+			var calculo2 = 1-Math.pow(calculo1,-240);
+			var totalPagar =  otro / calculo2;
+			//Prima
+			document.getElementById('resultadoPrima').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(resultadoPrima.toFixed(2));
+			//Monto a Financiar
 			document.getElementById('result').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(resultado.toFixed(2));
+			//Cuota
+			document.getElementById('cuota').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(totalPagar.toFixed(2));
 		}
 
 		if (prima === 'prima2') {
-			var resultado = precio2 * prima2;
+			//Prima
+			var resultadoPrima = precio2 * prima2;
+			console.log(resultadoPrima);
+			//calculo de cuota
+			var intereses = tasas2/12;
+			var resultado = precio2 - (precio2 * prima2);
+			var otro = intereses * resultado;
+			var calculo1 = 1+intereses;
+			var calculo2 = 1-Math.pow(calculo1,-240);
+			var totalPagar =  otro / calculo2;
+			//Prima
+			document.getElementById('resultadoPrima').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(resultadoPrima.toFixed(2));
+			//Monto a Financiar
 			document.getElementById('result').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(resultado.toFixed(2));
+			//Cuota
+			document.getElementById('cuota').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(totalPagar.toFixed(2));
 		}
 
 		if (prima === 'prima3') {
-			var resultado = precio2 * prima3;
+			//Prima
+			var resultadoPrima = precio2 * prima3;
+			console.log(resultadoPrima);
+			//calculo de cuota
+			var intereses = tasas2/12;
+			var resultado = precio2 - (precio2 * prima3);
+			var otro = intereses * resultado;
+			var calculo1 = 1+intereses;
+			var calculo2 = 1-Math.pow(calculo1,-240);
+			var totalPagar =  otro / calculo2;
+			//Prima
+			document.getElementById('resultadoPrima').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(resultadoPrima.toFixed(2));
+			//Monto a Financiar
 			document.getElementById('result').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(resultado.toFixed(2));
+			//Cuota
+			document.getElementById('cuota').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(totalPagar.toFixed(2));
 		}
 
 		if (prima === 'prima4') {
-			var resultado = precio2 * prima4;
+			//Prima
+			var resultadoPrima = precio2 * prima4;
+			console.log(resultadoPrima);
+			//calculo de cuota
+			var intereses = tasas2/12;
+			var resultado = precio2 - (precio2 * prima4);
+			var otro = intereses * resultado;
+			var calculo1 = 1+intereses;
+			var calculo2 = 1-Math.pow(calculo1,-240);
+			var totalPagar =  otro / calculo2;
+			//Prima
+			document.getElementById('resultadoPrima').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(resultadoPrima.toFixed(2));
+			//Monto a Financiar
 			document.getElementById('result').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(resultado.toFixed(2));
+			//Cuota
+			document.getElementById('cuota').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(totalPagar.toFixed(2));
 		}
 	}
 
@@ -139,10 +226,5 @@ function calc() {
 			document.getElementById('result').value = Intl.NumberFormat("en-IN", { style: "currency", currency: "USD", currencyDisplay: 'narrowSymbol' }).format(resultado.toFixed(2));
 		}
 	}
-
-
-
-	console.log(prima);
-	console.log(condominio);
 
 }
